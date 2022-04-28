249569814aa8a2750502f34f0845df865628da3e
Your Name <you@example.com>
2022-04-28 18:53:24 +0300

    change

---
+++ python.py
@@ -0,0 +1,12 @@
+print("Hello, welcome to the python chatbox!!")
+query1= input("Are you excited to interact with the program? Enter yes or no\n")
+if query1=='yes':
+    query2 = int(input("How old are you?\n"))
+    if query2 <=17:
+        print("Sorry this is not appropriate for you")
+    else:
+        query3=input("How are you feeling today?? enter good,nice, elated,okay,happy,angry,sad,lonely,pressured,frustrated\n")
+        if query3=="good" or query3=="okay" or query3=="happy" or query3=="elated":
+            print("Shine on you got this!!")
+        elif query3=="angry" or query3=="frustrated" or query3=="agitated" or query3=="on edge" or query3=="aaahhh":
+            print("Take a deep breath everything is going to be okay")
